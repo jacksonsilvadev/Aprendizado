2b49403ccf47df228bb7a17ee97b790b26bf8eeb
Jackson <jackson.rodrigues9@hotmail.com>
2019-01-24 15:47:27 -0200

    Preenchimento de contrato automatizado

---
+++ script.js
@@ -0,0 +1,13 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        EMPRESA_CONTRATANTE:' Empresa Fantasia ',
+        ENDERECO: 'Rua Fantasia, 0, Natal - RN, Brasil',
+        EMPRESA_CONTRATADA: ' Empresa jackinho.com',
+        ENDERECO_CONTRATADA: 'Rua jackinho, 15, São Paulo - SP, Brasil',
+        OBJETO: 'E-COMMERCE',
+        VALOR: 0,
+        VALOR_ESCRITO: 'Zero'
+    }
+});
+
