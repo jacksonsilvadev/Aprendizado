7f305d0646ed581d688cf788e08fb951a09661d3
Jackson <jackson.rodrigues9@hotmail.com>
2019-02-26 09:37:35 -0300

    Segunda aula do projeto Vuemastery easy - Utilização de v-bind e shorcut para ele que é ':'src por exemplo

--- Project couse vuemastery easy/main.js
+++ Project couse vuemastery easy/main.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'A pair of warm, fuzzy socks'
+        image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+        // link: 'https://www.vuejs.org'
     }
 })
