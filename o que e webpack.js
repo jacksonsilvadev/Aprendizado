44345c576a1c1f1267dbbc3027e8df51650328a0
Jackson <jackson.rodrigues9@hotmail.com>
2019-03-26 18:06:23 -0300

    Alterando e Aulas de webpack e SASS

---
+++ o que e webpack.js
@@ -0,0 +1,7 @@
+/*
+
+O QUE É WEBPACK?
+
+R: Feito para como aplicativo pegar vários arquivos separados do javascript,css e etc. Escanear tudo e estruturar todos eles da forma para ter uma melhor performance.
+
+*/
