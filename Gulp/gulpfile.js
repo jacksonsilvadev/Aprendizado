e78b30c7e8777c7eca4c2da1d97c3ec5b5a76663
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-03-27 05:02:17 -0300

    Gulp

---
+++ Gulp/gulpfile.js
@@ -0,0 +1,23 @@
+ /*
+ function helloTask(cb) {
+   
+   
+     cb()
+ }
+ 
+Esqueleto de uma função "vazia"
+
+ exports.default = helloTask;
+
+ */
+
+ function javascript (cb) {
+    cb()
+ }
+
+ function css(cb) {
+     cb()
+ }
+
+ exports.js = javascript;
+ exports.css = css
---
+++ JS/node teste/src/index.js
@@ -0,0 +1,5 @@
+const moment = require('moment');
+
+let dia = moment().format('dddd');
+
+console.log(dia);
