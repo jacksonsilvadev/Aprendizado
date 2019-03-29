c38193ec118ffa024392a3098958b1e2e165af8a
Jackson <jackson.rodrigues9@hotmail.com>
2019-03-29 17:01:10 -0300

    Retomando o Vue

---
+++ Revendo Vue/1 - O que é Vue e como usar/O que e Vue.js
@@ -0,0 +1,9 @@
+/*
+    Um Framework de JavaScript Progressive, aonde você pode construir qualquer coisa mesmo ja tendo criado o sistema visual ou não.
+
+    Muito famosa junto com o React e retirar do jogo o Jquery e React.
+
+    Instalando ela em nossos sistema
+
+    <script src="./vue.js"></script>
+*/
