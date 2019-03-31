c6a4a7cc71c086c685a99b1bbdae75c496ffe2f3
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-03-31 15:06:46 -0300

    Feito até 30 min projeto Todo List

---
+++ vue_crash_todolist/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+   <Todos v-bind:todos="todos"></Todos>
+  </div>
+</template>
+
+<script>
+
+import Todos from './components/Todos.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Todos
+  },
+  data() {
+    return{
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+         {
+          id: 2,
+          title: "Todo Two",
+          completed: true
+        },
+         {
+          id: 3,
+          title: "Todo Theree",
+          completed: false
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
