2929ee5a8b010a3befe3dc04f64d64dba27a1616
Jackson <jackson.rodrigues9@hotmail.com>
2019-04-09 17:30:13 -0300

    Animations e transitions

---
+++ vue.js_especializa/vue-animation-transition/src/components/animations/Question.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2 v-text="question"></h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      question: "Gerando pergunta ..."
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ vue.js_especializa/vue-animation-transition/src/components/animations/Quiz.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2 v-text="title"></h2>
+
+    <app-question></app-question>
+  </div>
+</template>
+
+<script>
+import Question from "./Question";
+
+export default {
+  data() {
+    return {
+      title: "Quiz"
+    };
+  },
+  components: {
+    "app-question": Question
+  }
+};
+</script>
+
+<style scoped>
+</style>
