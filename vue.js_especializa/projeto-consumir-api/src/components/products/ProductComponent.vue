dc2ac1bbc87604d62b709cf5432b93da8b54b78d
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-04-11 21:35:48 -0300

    Adicionado preloader no projeto consumir api

--- vue.js_especializa/projeto-consumir-api/src/components/products/ProductComponent.vue
+++ vue.js_especializa/projeto-consumir-api/src/components/products/ProductComponent.vue
@@ -1,6 +1,32 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
+
+    <table class="table table-dark">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Nome</th>
+          <th>Descrição</th>
+          <th width="200">Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(product, index) in products.data" :key="index">
+          <th v-text="product.id"></th>
+          <th v-text="product.name"></th>
+          <th v-text="product.description"></th>
+          <th>
+            <a href="#" class="btn btn-info">Editar</a>
+            <a href="#" class="btn btn-danger">Deletar</a>
+          </th>
+        </tr>
+      </tbody>
+    </table>
+
+    <div v-if="preloader">
+      <img src="../../assets/preloader.gif" alt="preloader" class="preloader">
+    </div>
   </div>
 </template>
 
@@ -8,8 +34,27 @@
 export default {
   data() {
     return {
-      title: "Product Page do Site"
+      title: "Product Page do Site",
+      products: {},
+      preloader: false
     };
+  },
+  methods: {
+    getProducts () {
+      this.preloader=true
+      this.$http.get('http://laravel55-weservice.local/api/v1/products')
+      .then(response => {
+        this.products = response.body
+      this.preloader = false
+
+      }), error => {
+        console.log(error)
+      }
+    }
+  },
+  created(){
+
+    this.getProducts()
   }
 };
 </script>
